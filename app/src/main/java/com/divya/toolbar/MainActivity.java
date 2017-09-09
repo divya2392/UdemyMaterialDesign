c4379662003b8ac421072920dee1c554503be6f9
Divya Gupta <divagupta2392@gmail.com>
2017-09-09 14:56:59 +0530

    Initial commit

---
+++ app/src/main/java/com/divya/toolbar/MainActivity.java
@@ -0,0 +1,49 @@
+package com.divya.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id== R.id.item1_id)
+        {
+            Toast.makeText(this, "item 1 clicked", Toast.LENGTH_SHORT).show();
+
+        }else if(id== R.id.item2_id)
+        {
+            Toast.makeText(this, "item 2 clicked", Toast.LENGTH_SHORT).show();
+
+        }else if(id== R.id.item3_id)
+        {
+            Toast.makeText(this, "item 3 clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
